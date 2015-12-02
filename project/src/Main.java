4a4c5347661ace2a0391d1dc83deebe6f2040891
warreee <wardschodts@hotmail.com>
2015-12-02 19:57:24 +0100

    first query program

--- project/src/Main.java
+++ project/src/Main.java
@@ -1,14 +1,21 @@
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
+import org.apache.jena.query.*;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+
+import java.io.*;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // Open the bloggers RDF graph from the filesystem
-        InputStream in = new FileInputStream(new File("bloggers.rdf"));
+        InputStream in = null;
+        try {
+            in = new FileInputStream(new File("bloggers.rdf"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
         // Create an empty in-memory model and populate it from the graph
-        Model model = ModelFactory.createMemModelMaker().createModel();
+        Model model = ModelFactory.createMemModelMaker().createModel("test");
         model.read(in, null); // null base URI, since model URIs are absolute
         in.close();
         // Create a new query
