c27c44a3e1af1b4aa699ac5de4ba7cf5cfd24789
DuctrTape <gustverbruggen@gmail.com>
2015-12-09 14:17:53 +0100

    no message

---
+++ project/scripts/getRankings.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/perl
+
+use LWP::Simple qw(get);
+
+($out) = @ARGV;
+if (not defined $out) {
+	$out = 'rankings.csv';
+}
+
+$html = get "http://www.shanghairanking.com/ARWU2015.html";
+
+# Parse table
+$th = 'name="UniversityRanking">';
+$tf = '<\/table>';
+$html =~ /$th(.+)$tf/s;
+$table = $1;
+
+# Parse rows
+@rows = $table =~  /class="bgf[5d]">(.*?)<tr/g;
+
+open $f, '>', $out;
+foreach $r (@rows) {
+
+	# Parse cells
+	@cells = $r =~ /<td.*?>(.*?)<\/td>/g;
+
+	# Get university name
+	$cells[1] =~ /<a.*?>(.*?)<\/a>/;
+	$name = $1;
+
+	# Get award score
+	$cells[6] =~ /<div>(.*?)\s<\/div>/;
+	$score = $1;
+
+	# Write to file
+	print $f "$name,$score\n";
+
+}
+close $f;
