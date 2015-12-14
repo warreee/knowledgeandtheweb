ae92f03798dd205b1e0a48cc9a1de99d4b6c0154
warreee <ward.schodts@student.kuleuven.be>
2015-12-14 01:49:47 +0100

    ToE politiekers

--- project/src/Main.java
+++ project/src/Main.java
@@ -3,119 +3,78 @@ import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 
 import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class Main {
 
+    private List<String> strings = new ArrayList<>();
+
     public static void main(String[] args) throws IOException {
-        test2();
+        outerQuery();
     }
 
-    private void test1() throws IOException {
-        // Open the bloggers RDF graph from the filesystem
-        InputStream in = null;
-        try {
-            in = new FileInputStream(new File("bloggers.rdf"));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        // Create an empty in-memory model and populate it from the graph
-        Model model = ModelFactory.createMemModelMaker().createModel("test");
-        model.read(in, null); // null base URI, since model URIs are absolute
-        in.close();
-        // Create a new query
-        String queryString =
-                "PREFIX foaf: <http://xmlns.com/foaf/0.1/> " +
-                        "SELECT ?url " +
-                        "WHERE {" +
-                        " ?contributor foaf:name \"Jon Foobar\" . " +
-                        " ?contributor foaf:weblog ?url . " +
-                        " }";
+    private static void outerQuery() throws IOException {
+        String queryString = readFile(System.getProperty("user.dir") + "/queries/ToEpoliticians.rq", Charset.defaultCharset());
         Query query = QueryFactory.create(queryString);
-        // Execute the query and obtain results
-        QueryExecution qe = QueryExecutionFactory.create(query, model);
-        ResultSet results = qe.execSelect();
-        // Output query results
-        ResultSetFormatter.out(System.out, results, query);
-        // Important - free up resources used running the query
-        qe.close();
-    }
-
-    private static  void test2() {
-        String queryString=
-        "PREFIX dbpedia-owl: <http://dbpedia.org/ontology/>"+
-        "PREFIX nobel: <http://data.nobelprize.org/terms/>"+
-        "PREFIX foaf: <http://xmlns.com/foaf/0.1/>"+
-        "PREFIX yago: <http://yago-knowledge.org/resource/>"+
-        "PREFIX viaf: <http://viaf.org/viaf/>"+
-        "PREFIX meta: <http://www4.wiwiss.fu-berlin.de/bizer/d2r-server/metadata#>"+
-        "PREFIX dcterms: <http://purl.org/dc/terms/>"+
-        "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>"+
-        "PREFIX d2r: <http://sites.wiwiss.fu-berlin.de/suhl/bizer/d2r-server/config.rdf#>"+
-        "PREFIX dbpedia: <http://dbpedia.org/resource/>"+
-        "PREFIX owl: <http://www.w3.org/2002/07/owl#>"+
-        "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>"+
-        "PREFIX map: <http://data.nobelprize.org/resource/#>"+
-        "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>"+
-        "PREFIX freebase: <http://rdf.freebase.com/ns/>"+
-        "PREFIX dbpprop: <http://dbpedia.org/property/>"+
-        "PREFIX skos: <http://www.w3.org/2004/02/skos/core#>" +
-                "SELECT DISTINCT ?label"+
-       " WHERE {"+
-            "?laur rdf:type nobel:Laureate ."+
-                    "?laur rdfs:label ?label ."+
-                    "?laur dbpedia-owl:birthPlace <http://data.nobelprize.org/resource/country/Iceland> .}";
-
-        // now creating query object
-        Query query = QueryFactory.create(queryString);
-        // initializing queryExecution factory with remote service.
-        // **this actually was the main problem I couldn't figure out.**
-        QueryExecution qexec = QueryExecutionFactory.sparqlService("http://data.nobelprize.org/sparql", query);
-
-        //after it goes standard query execution and result processing which can
-        // be found in almost any Jena/SPARQL tutorial.
-        try {
-            ResultSet results = qexec.execSelect();
-            System.out.println(results.getResultVars().size());
-
-            for ( ; results.hasNext();) {
-                QuerySolution solution = results.nextSolution();
-                System.out.println(solution.getLiteral("label") + " " );
-
-            }
-        }
-        finally {
-            qexec.close();
-        }
-    }
-
-    private static  void test3() {
-        String queryString=
-                "PREFIX foaf:<http://xmlns.com/foaf/0.1/> " +
-                        "SELECT ?firstname ?lastname WHERE { " +
-                        "  ?speaker foaf:givenName ?firstname." +
-                        "  ?speaker foaf:familyName ?lastname." +
-                        "}";
-
-        // now creating query object
-        Query query = QueryFactory.create(queryString);
-        // initializing queryExecution factory with remote service.
-        // **this actually was the main problem I couldn't figure out.**
         QueryExecution qexec = QueryExecutionFactory.sparqlService("http://linkedpolitics.ops.few.vu.nl/sparql/", query);
 
-        //after it goes standard query execution and result processing which can
-        // be found in almost any Jena/SPARQL tutorial.
         try {
             ResultSet results = qexec.execSelect();
-            System.out.println(results.getResultVars().size());
 
-            for ( ; results.hasNext();) {
+            for (; results.hasNext(); ) {
                 QuerySolution solution = results.nextSolution();
-                System.out.println(solution.getLiteral("firstname") + " " + solution.getLiteral("lastname"));
+
+                    innerQuery(solution.get("person").toString(), "http://dbpedia.org/sparql/");
+
 
             }
-        }
-        finally {
+        } finally {
             qexec.close();
         }
+
     }
+
+    private static void innerQuery(String resource, String service) throws IOException {
+        String queryString = replaceResource(resource);
+        Query query = QueryFactory.create(queryString);
+        QueryExecution qexec = QueryExecutionFactory.sparqlService(service, query);
+
+        try {
+            ResultSet results = qexec.execSelect();
+            System.out.println(resource);
+            for (; results.hasNext(); ) {
+                QuerySolution solution = results.nextSolution();
+
+                System.out.println(solution);
+
+            }
+        } finally {
+            qexec.close();
+        }
+
+    }
+
+    private static List<String> readLines(String path, Charset encoding) throws IOException {
+        return Files.readAllLines(Paths.get(path), encoding);
+
+    }
+
+    private static String replaceResource(String resource) throws IOException{
+        return readLines(System.getProperty("user.dir") + "/queries/specificResource.rq", Charset.defaultCharset())
+                .stream().map(s -> s.replace("$RESOURCE$", resource)).collect((Collectors.joining("\n")));
+    }
+
+
+    private static String readFile(String path, Charset encoding)
+            throws IOException {
+        byte[] encoded = Files.readAllBytes(Paths.get(path));
+        return new String(encoded, encoding);
+    }
+
 }
