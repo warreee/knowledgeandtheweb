963f88fdcdfb7d50e9feff2e6568b05ea1c65801
DuctrTape <gustverbruggen@gmail.com>
2015-12-28 10:23:51 +0100

    wut

--- project/scripts/getScholarRankings.pl
+++ project/scripts/getScholarRankings.pl
@@ -2,11 +2,20 @@
 
 open $file, '<', "../data/names.csv";
 open $out, '>', "../data/scholarrankings.csv";
+open $prox, '<', "proxies3.txt";
+
+@proxies;
+while (<$prox>) {
+  chomp;
+  push @proxies, $_;
+}
+
 
 $i = 0;
 while (<$file>) {
   chomp;
-  $n = `perl getScholarRanking.pl "$_"`;
+  $proxy = $proxies[rand @proxies];
+  $n = `perl getScholarRanking.pl \"$_\" http://$proxy`;
   print $out "$n\n";
   print "$i\t$_\t$n\n";
   $i++;
