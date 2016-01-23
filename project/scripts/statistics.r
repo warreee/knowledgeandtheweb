cbba56e1125496889aebb1cbed7b347d9937ca0f
DuctrTape <gustverbruggen@gmail.com>
2016-01-24 00:19:00 +0100

    Model. Figures for statistics.

--- project/scripts/statistics.r
+++ project/scripts/statistics.r
@@ -1,52 +1,86 @@
+library(tikzDevice)
+library(plyr)
+
+# Read file
 nobelAll.df <- read.table('finalData.csv', sep = ';', header = TRUE,  quote = "");
+# Remove missing values
 nobel.df <- na.omit(nobelAll.df)
 
-plot(density(log(nobel.df$Popularity)))
+##############
+# POPULARITY #
+##############
 
-n <- length(nobel.df$Popularity)
-sort(nobel.df$Popularity, partial=n-16)[n-16]
-
-NewPop <- nobel.df$Popularity[which(nobel.df$Popularity < 175000)]
-
-popBp <- boxplot.stats(nobel.df$Popularity, coef = 3)
-summary(popBp)
-plot(popBp)
-
-
-
-nobel.popularity.bp <- boxplot.stats(nobel.df$Popularity)
-plot(density(nobel.df$Popularity, na.rm = TRUE))
-
-par(mfrow = c(2,2))
-by(nobel.df$Productivity, nobel.df$Type, function(x) plot(density(x)))
-by(nobel.df$Popularity, nobel.df$Type, function(x) plot(density(x)))
-
-# Normalise by category
-by(nobel.df$Productivity, nobel.df$Type, function(x) (x / max(x, na.rm = TRUE)));
-by(nobel.df$Popularity, nobel.df$Type, function(x) (x / max(x, na.rm = TRUE)));
-
-# Scholar <- Scholar[rowSums(is.na(Scholar)) == 0,];
-# ScholarRankings <- Scholar$V2;
-# ScholarRankings <- (ScholarRankings - min(ScholarRankings)) / (max(ScholarRankings - min(ScholarRankings)))
+# # Boxplot of popularity
+# tikz('../paper/figures/initialBox.tex', standAlone = TRUE)
+# boxplot(nobel.df$Popularity, axes = F)
+# axis(2,
+#      at = c(0, 5e+06,              1e+07,    1.5e+07),
+#  labels = c(0, "$5 \\times 10^6$", "$10^7$", "$1.5 \\times 10^7$"))
+# box()
+# dev.off()
 # 
-# Speeches <- read.table('numberofspeeches.csv', sep = ',', header = TRUE, quote = "");
-# SpeechesRankings <- Speeches$count;
-# SpeechesRankings <- (SpeechesRankings - min(SpeechesRankings)) / (max(SpeechesRankings - min(SpeechesRankings)))
+# # Density plot of popularity
+# tikz('../paper/figures/initialDensity.tex', standAlone = TRUE)
+# plot(density(nobel.df$Popularity), main = "",
+#      axes = FALSE,
+#      zero.line = false)
+# axis(1, at = c(0, 10000000, max(nobel.df$Popularity)), 
+#     labels = c(0, "$10^7$",   "$17 \\cdot 10^7$"))
+# axis(2)
+# box()
+# dev.off()
+
+# Log-transform popularity
+nobel.df$LogPopularity = log(nobel.df$Popularity)
+nobel.df$LogPopularity[nobel.df$LogPopularity == -Inf] <- 0
+
+# # Density and boxplot of log-transformed popularity
+# tikz('../paper/figures/transformedBox.tex', standAlone = TRUE)
+# boxplot(nobel.df$LogPopularity, las = 2)
+# dev.off()
 # 
-# boxplot(SpeechesRankings);
-# boxplot(ScholarRankings);
+# tikz('../paper/figures/transformedDensity.tex', standAlone = TRUE)
+# plot(density(nobel.df$LogPopularity), main = "",
+#      zero.line = FALSE)
+# dev.off()
+
+# Standardise by category
+nobel.df$LogPopularityStand <- ave(nobel.df$LogPopularity, 
+                                   nobel.df$Type, 
+                                   FUN = function(x) x / max(x))
+
+# Test if there is sufficient difference in means
+t.test(nobel.df$LogPopularityStand[nobel.df$Nobel == "yes"],
+       nobel.df$LogPopularityStand[nobel.df$Nobel == "no"])
+# Luckily -> yes.
+
+################
+# PRODUCTIVITY #
+################
+
+# tikz('../paper/figures/prodInitialBox.tex', standAlone = TRUE)
+# boxplot(nobel.df$Productivity)
+# dev.off()
 # 
-# par(mfrow=c(1,2));
-# plot(density(ScholarRankings), main = 'Scholar');
-# plot(density(SpeechesRankings), main = 'Speeches');
-# 
-# # remove outliers
-# x1 <- ScholarRankings[!ScholarRankings %in% boxplot.stats(ScholarRankings)$out];
-# x2 <- SpeechesRankings[!SpeechesRankings %in% boxplot.stats(SpeechesRankings)$out];
-# 
-# boxplot(x1);
-# boxplot(x2);
-# 
-# par(mfrow=c(1,2));
-# plot(density(x1), main = 'Scholar');
-# plot(density(x2), main = 'Speeches');
+# tikz('../paper/figures/prodInitialDensity.tex', standAlone = TRUE)
+# plot(density(nobel.df$Productivity))
+# dev.off()
+
+# Normalise popularity by category
+nobel.df$ProductivityStand <- ave(nobel.df$Productivity, 
+                                  nobel.df$Type, 
+                                  FUN = function(x) x / max(x))
+
+# Verify whether the means are different
+t.test(nobel.df$ProductivityStand[nobel.df$Nobel == "yes"],
+       nobel.df$ProductivityStand[nobel.df$Nobel == "no"])
+# Luckily -> yes (although only slightly).
+
+# Save resulting files
+variables <- c("Nobel", "Country", "Year", "UniversityScore", 
+               "LogPopularityStand", "ProductivityStand")
+nobel.subs <- nobel.df[variables]
+names(nobel.subs)[5] = "Popularity"
+names(nobel.subs)[6] = "Productivity"
+write.table(nobel.subs, "trainingSet.csv", sep = ";",
+            row.names = FALSE)
